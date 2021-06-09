6a390a6b6cf59aa6381beb52507f006567bc7a74
yportfolio <wangyeux@gmail.com>
2021-06-09 16:26:10 +0800

    reset to the book's version

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri:
+    process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    "mongodb+srv://ye:wy6524615@cluster0.mbuii.mongodb.net/mern-infra?retryWrites=true&w=majority",
+};
+
+export default config;

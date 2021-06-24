3aa5fab3f38077f0127abdc60adb7eadaad3e9c7
wangyes <wangyes@yonyou.com>
2021-06-25 00:55:55 +0800

    bug: Fixed a problem that a controller parameter is wrong because the wrong route sequence

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,3 +1,6 @@
+/* 
+Route order is fucking important!!!
+ */
 import express from "express";
 import userCtrl from "../controllers/user.controller";
 import authCtrl from "../controllers/auth.controller";
@@ -5,15 +8,6 @@ import authCtrl from "../controllers/auth.controller";
 const router = express.Router();
 
 router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
-
-/* 
-A route can has multiple handlers that will be executed one by one
-*/
-router
-  .route("/api/users/:userId")
-  .get(authCtrl.requireSignin, userCtrl.read)
-  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
-  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
 /* 
 If found photo, send it in the response to the request. Otherwise, send default photo instead.
@@ -26,6 +20,34 @@ router
 Send default photo  to the request. 
  */
 router.route("/api/users/defaultphoto").get(userCtrl.defaultPhoto);
+
+/* 
+Add one to following and follower array 
+ */
+router
+  .route("/api/users/follow")
+  .put(authCtrl.requireSignin, userCtrl.addFollowing, userCtrl.addFollower);
+
+/* 
+Remove one from following and follower array 
+ */
+router
+  .route("/api/users/unfollow")
+  .put(
+    authCtrl.requireSignin,
+    userCtrl.removeFollowing,
+    userCtrl.removeFollower
+  );
+
+/* 
+A route can has multiple handlers that will be executed one by one
+*/
+router
+  .route("/api/users/:userId")
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
+
 /* Add userByID callback triggered by the userId parameter.
 Add user data to req.profile 
 */

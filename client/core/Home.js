430c0670e18cf34717c43bf6ae4570ae1a26e6e4
wangyes <wangyes@yonyou.com>
2021-07-13 19:44:02 +0800

    feat: complete the home page

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import unicornbikeImg from "./../assets/images/unicornbike.jpg";
-import FindPeople from "../user/FindPeople";
 import Grid from "@material-ui/core/Grid";
-import auth from "../auth/auth-helper";
-import Newsfeed from "../post/Newsfeed";
+import auth from "./../auth/auth-helper";
+import FindPeople from "./../user/FindPeople";
+import Newsfeed from "./../post/Newsfeed";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,49 +41,67 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const signin = auth.isAuthenticated();
-
-export default function Home() {
+export default function Home({ history }) {
   const classes = useStyles();
+  const [defaultPage, setDefaultPage] = useState(false);
+
+  useEffect(() => {
+    setDefaultPage(auth.isAuthenticated());
+    //Listen for changes to the current location
+    const unlisten = history.listen(() => {
+      setDefaultPage(auth.isAuthenticated());
+    });
+
+    //Clearing up the listener
+    return () => {
+      unlisten();
+    };
+  }, []);
+
   return (
     <div className={classes.root}>
-      <Card className={classes.card}>
-        <Typography variant="h6" className={classes.title}>
-          Home Page
-        </Typography>
-        <CardMedia
-          className={classes.media}
-          image={unicornbikeImg}
-          title="Unicorn Bicycle"
-        />
-        <Typography
-          variant="body2"
-          component="p"
-          className={classes.credit}
-          color="textSecondary"
-        >
-          Photo by
-          <a
-            href="https://unsplash.com/@boudewijn_huysmans"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Boudewijn Huysmans
-          </a>
-          on Unsplash
-        </Typography>
-        <CardContent>
-          <Typography variant="body1" component="p">
-            Welcome to the MERN Infra home page.
+      {!defaultPage && (
+        <Card className={classes.card}>
+          <Typography variant="h6" className={classes.title}>
+            Home Page
           </Typography>
-        </CardContent>
-      </Card>
-      {signin && (
+          <CardMedia
+            className={classes.media}
+            image={unicornbikeImg}
+            title="Unicorn Bicycle"
+          />
+          <Typography
+            variant="body2"
+            component="p"
+            className={classes.credit}
+            color="textSecondary"
+          >
+            Photo by
+            <a
+              href="https://unsplash.com/@boudewijn_huysmans"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Boudewijn Huysmans
+            </a>
+            on Unsplash
+          </Typography>
+          <CardContent>
+            <Typography variant="body1" component="p">
+              Welcome to the MERN Infra home page.
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
+
+      {defaultPage && (
         <Grid container space={8}>
-          <Grid item xs={6} sm={5}>
+          <Grid item xs={8} sm={7}>
             <FindPeople />
           </Grid>
-          <Newsfeed />
+          <Grid item xs={6} sm={5}>
+            <Newsfeed />
+          </Grid>
         </Grid>
       )}
     </div>
